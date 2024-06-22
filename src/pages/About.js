a82f8bebeb43da97d0b974cfb7eb859590a0ecbf
vundi <vundi4773@gmail.com>
2024-06-22 21:21:38 +0300

    routes

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+function About() {
+  return (
+    <div>
+        <header>
+       <NavBar/>
+      </header>
+      <main>
+        <h1>This is my about component!</h1>
+      </main>
+    </div>
+  )
+}
+
+export default About
